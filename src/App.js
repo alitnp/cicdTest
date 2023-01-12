99067bbd11daf6b9ddec51ccbbf742dc8db113c6
hamedani.a <aliagha367@gmail.com>
2023-01-12 11:00:09 +0330

    test workflow

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	console.log("hello world2");
+	console.log("hello world3");
 	return (
 		<div className="App">
 			<header className="App-header">
